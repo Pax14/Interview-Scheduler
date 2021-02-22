d790e02e1e5b51fa050fb2df40504694e3eb5d52
Paxon Cheung <paxon.cheung@gmail.com>
2021-02-21 21:22:42 -0500

    Rendering static interview page

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "components/DayList"
+import Appointment from "components/Appointment/index"
 
 const days = [
   {
@@ -21,9 +22,66 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Paxon Cheung",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Sabrina Yau",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: "last",
+  }
+];
+
 export default function Application(props) {
 
   const [day, setDay] = useState('Monday');
+
+  // function to render all appts
+  const schedule = appointments.map(appointment => {
+    return (
+      <Appointment key={appointment.id} id={appointment.id} time={appointment.time} interview={appointment.interview} />
+    );
+  });
 
   return (
     <main className="layout">
@@ -48,7 +106,7 @@ export default function Application(props) {
       />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {schedule}
       </section>
     </main>
   );
