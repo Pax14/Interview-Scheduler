1feb1bb412fc579eb54b36d319942d8b7859b0f1
Paxon Cheung <paxon.cheung@gmail.com>
2021-03-01 02:02:25 -0500

    finally got git working... updated retrieving interview data

---
+++ src/helpers/selectors.js
@@ -0,0 +1,27 @@
+export function getAppointmentsForDay(state, day) {
+  //... returns an array of appointments for that day
+  const filteredDays = state.days.filter(singleDay => { 
+    return singleDay.name === day
+    })
+
+  if (filteredDays.length === 0) {
+    return [];
+  }
+
+  const appointments = filteredDays[0].appointments.map((app) => {
+    return state.appointments[app]
+  })
+
+  return appointments;
+}
+
+export function getInterview(state, interview) {
+
+  if (!interview) {
+    return null;
+  }
+
+  const interviewerId = interview.interviewer;
+  const interviewer = state.interviewers[interviewerId];
+  return {...interview, interviewer: interviewer};
+}
