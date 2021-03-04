c8f6d7d1d7ee5cfb28cb021867ba4c6ffdce495c
Paxon Cheung <paxon.cheung@gmail.com>
2021-03-04 08:58:00 -0500

    creating appointment tests, cancel interview and increase count by 1

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, cleanup, waitForElement, fireEvent, getByText, getAllByTestId, getByAltText, getByPlaceholderText, queryByText } from "@testing-library/react";
 import Application from "components/Application";
-import axios from 'axios'
+
 
 afterEach(cleanup);
 
